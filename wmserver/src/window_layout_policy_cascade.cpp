a98dbebfc3c2999ff8c0024c02e8152a79e75442
xingyanan <xingyanan2@huawei.com>
2022-03-25 12:05:28 +0800

    fixed b47c807 from https://gitee.com/bug_xyn/windowmanager/pulls/649 修复告警
    Signed-off-by: xingyanan <xingyanan2@huawei.com>
    Change-Id: I62358efbdaca858f74186c5afa497a380e4a9b4d

--- wmserver/src/window_layout_policy_cascade.cpp
+++ wmserver/src/window_layout_policy_cascade.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -188,6 +188,10 @@ void WindowLayoutPolicyCascade::InitCascadeRect()
 
 void WindowLayoutPolicyCascade::UpdateLayoutRect(sptr<WindowNode>& node)
 {
+    if (node == nullptr) {
+        WLOGFE("window node is nullptr.");
+        return;
+    }
     auto type = node->GetWindowType();
     auto mode = node->GetWindowMode();
     auto property = node->GetWindowProperty();
