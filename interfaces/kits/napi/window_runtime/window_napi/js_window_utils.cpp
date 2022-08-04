1a8b2acab316a156ebe2a447dec0c90db360018d
ouliang333 <ouliangliang@huawei.com>
2022-08-04 08:06:15 +0000

    Added the interface for obtaining window ids
    Signed-off-by: ouliang333 <ouliangliang@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window_utils.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_utils.cpp
@@ -328,7 +328,7 @@ NativeValue* CreateJsWindowPropertiesObject(NativeEngine& engine, sptr<Window>& 
     object->SetProperty("isTransparent", CreateJsValue(engine, window->IsTransparent()));
     object->SetProperty("isRoundCorner", CreateJsValue(engine, false)); // empty method
     object->SetProperty("dimBehindValue", CreateJsValue(engine, 0));
-    object->SetProperty("windowsId", CreateJsValue(engine, window->GetWindowId()));
+    object->SetProperty("windowId", CreateJsValue(engine, window->GetWindowId()));
     return objValue;
 }
 static std::string GetHexColor(uint32_t color)
