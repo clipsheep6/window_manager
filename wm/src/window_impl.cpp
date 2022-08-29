f7b9a991763549db6e600f8ef03026511b1472e2
yangfei <yangfei110@huawei.com>
2022-08-29 12:19:35 +0800

    set icon and label to uicontent
    Signed-off-by: yangfei <yangfei110@huawei.com>
    Change-Id: Ia5dfc6b7aa286cab72e3b510f6ee7d8ec3419f3c
    Signed-off-by: yangfei <yangfei110@huawei.com>

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -2869,6 +2869,27 @@ void WindowImpl::GetRequestedTouchHotAreas(std::vector<Rect>& rects) const
     property_->GetTouchHotAreas(rects);
 }
 
+WMError WindowImpl::SetAPPWindowLable(const std::string& label) 
+{
+    if(uiContent_==nullptr){
+        WLOGFI("uicontent is empty");
+        return WMError::WM_ERROR_NULLPTR;
+    }
+    WLOGFI("SetAPPWindowLable");
+}
+WMError WindowImpl::SetAPPWindowIcon(shared_ptr<Media::PixelMap>& icon)
+{
+    if(icon==nullptr){
+        WLOGFI("window icon is empty");
+        return WMError::WM_ERROR_NULLPTR;
+    }
+    if(uiContent_==nullptr){
+        WLOGFI("uicontent is empty");
+        return WMError::WM_ERROR_NULLPTR;
+    }
+    WLOGFI("SetAPPWindowIcon");
+}
+
 bool WindowImpl::CheckCameraFloatingWindowMultiCreated(WindowType type)
 {
     if (type != WindowType::WINDOW_TYPE_FLOAT_CAMERA) {
