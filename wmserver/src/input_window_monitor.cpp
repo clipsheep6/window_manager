2882a399f05d863b68100cda2aa6e19ba0053afb
zhirong <215782872@qq.com>
2022-09-20 10:06:18 +0800

    solution for DTS2022091600859
    Signed-off-by: zhirong <215782872@qq.com>
    Change-Id: I239921e8b1415d4f8ba3c787e79eafc61f342351

--- wmserver/src/input_window_monitor.cpp
+++ wmserver/src/input_window_monitor.cpp
@@ -148,7 +148,7 @@ void InputWindowMonitor::TransformWindowRects(const sptr<WindowNode>& windowNode
         for (Rect& rect : pointerHotAreas) {
             rect = WindowHelper::TransformRect(windowNode->GetWindowProperty()->GetTransformMat(), rect);
         }
-        WLOGFI("area rect befoe tranform: [%{public}d, %{public}d, %{public}u, %{public}u]",
+        WLOGFI("area rect before tranform: [%{public}d, %{public}d, %{public}u, %{public}u]",
             areaRect.posX_, areaRect.posY_, areaRect.width_, areaRect.height_);
         areaRect = WindowHelper::TransformRect(windowNode->GetWindowProperty()->GetTransformMat(), areaRect);
         WLOGFI("area rect after tranform: [%{public}d, %{public}d, %{public}u, %{public}u]",
