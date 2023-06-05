b10279a1be0a8b06cb72337c4a1f333894b231f7
lw19901203 <liuwei742@huawei.com>
2023-06-05 21:04:59 +0800

    modify napi out path
    Change-Id: Ie46c7283e83aa2fa85c570762d0e2275d2aa66a2

--- interfaces/kits/napi/window_runtime/window_stage_napi/js_window_stage.cpp
+++ interfaces/kits/napi/window_runtime/window_stage_napi/js_window_stage.cpp
@@ -544,9 +544,13 @@ NativeValue* CreateJsWindowStage(NativeEngine& engine, std::shared_ptr<Rosen::Wi
     WLOGFD("[NAPI]CreateJsWindowStage");
     NativeValue* objValue = engine.CreateObject();
     NativeObject* object = ConvertNativeValueTo<NativeObject>(objValue);
+    assert(object != nullptr);
 
+    WLOGFD("[NAPI]CreateJsWindowStage mlx 548");
     std::unique_ptr<JsWindowStage> jsWindowStage = std::make_unique<JsWindowStage>(windowScene);
+    WLOGFD("[NAPI]CreateJsWindowStage mlx 549");
     object->SetNativePointer(jsWindowStage.release(), JsWindowStage::Finalizer, nullptr);
+    WLOGFD("[NAPI]CreateJsWindowStage mlx 550");
 
     const char *moduleName = "JsWindowStage";
     BindNativeFunction(engine,
