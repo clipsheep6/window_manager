c4c491d0981fed5acb978c65acbeb4ad1efef8f1
z30042987 <zhouminghui8@huawei.com>
2023-06-13 10:43:30 +0800

    unnittest commit
    Signed-off-by: z30042987 <zhouminghui8@huawei.com>

--- window_scene/test/unittest/session_proxy_test.cpp
+++ window_scene/test/unittest/session_proxy_test.cpp
@@ -23,54 +23,81 @@ using namespace testing::ext;
 
 namespace OHOS {
 namespace Rosen {
-class ScreenSessionManagerTest : public testing::Test {
+class SessionProxyTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
     void SetUp() override;
     void TearDown() override;
 
-    static sptr<ScreenSessionManager> ssm_;
+    static sptr<Session> sessionProxy_;
+    // static sptr<SessionProxy> sessionProxy_;
 };
 
-sptr<ScreenSessionManager> ScreenSessionManagerTest::ssm_ = nullptr;
+sptr<Session> SessionProxyTest::ssn_ = nullptr;
 
-void ScreenSessionManagerTest::SetUpTestCase()
+void SessionProxyTest::SetUpTestCase()
 {
-    ssm_ = new ScreenSessionManager();
+
+    // sptr<WindowOption> option = new WindowOption();
+    // sptr<WindowSceneSessionImpl> windowsession = new WindowSceneSessionImpl(option);
+    SessionInfo info;
+    info.abilityName_ = "testSession1";
+    info.moduleName_ = "testSession2";
+    info.bundleName_ = "testSession3";
+    session_ = new (std::nothrow) Session(info);
+    sessionProxy_ = new SessionProxy(session_);
+    // ssn_ = new Session();
 }
 
-void ScreenSessionManagerTest::TearDownTestCase()
+void SessionProxyTest::TearDownTestCase()
 {
-    ssm_ = nullptr;
+    sessionProxy_ = nullptr;
 }
 
-void ScreenSessionManagerTest::SetUp()
+void SessionProxyTest::SetUp()
 {
 }
 
-void ScreenSessionManagerTest::TearDown()
+void SessionProxyTest::TearDown()
 {
 }
 
 namespace {
 /**
- * @tc.name: RegisterDisplayManagerAgent
+ * @tc.name: UpdateActiveStatus
  * @tc.desc: ScreenSesionManager rigister display manager agent
  * @tc.type: FUNC
  */
-HWTEST_F(ScreenSessionManagerTest, RegisterDisplayManagerAgent, Function | SmallTest | Level3)
+HWTEST_F(SessionProxyTest, UpdateActiveStatus01, Function | SmallTest | Level3)
 {
-    sptr<IDisplayManagerAgent> displayManagerAgent = new DisplayManagerAgentDefault();
-    DisplayManagerAgentType type = DisplayManagerAgentType::DISPLAY_STATE_LISTENER;
+    // sptr<IDisplayManagerAgent> displayManagerAgent = new DisplayManagerAgentDefault();
+    // DisplayManagerAgentType type = DisplayManagerAgentType::DISPLAY_STATE_LISTENER;
 
-    ASSERT_EQ(DMError::DM_ERROR_NULLPTR, ssm_->RegisterDisplayManagerAgent(nullptr, type));
-    ASSERT_EQ(DMError::DM_ERROR_NULLPTR, ssm_->UnregisterDisplayManagerAgent(nullptr, type));
+    // ASSERT_EQ(DMError::DM_ERROR_NULLPTR, sessionProxy_->RegisterDisplayManagerAgent(nullptr, type));
+    // ASSERT_EQ(DMError::DM_ERROR_NULLPTR, sessionProxy_->UnregisterDisplayManagerAgent(nullptr, type));
 
-    ASSERT_EQ(DMError::DM_ERROR_NULLPTR, ssm_->UnregisterDisplayManagerAgent(displayManagerAgent, type));
+    // ASSERT_EQ(DMError::DM_ERROR_NULLPTR, ssn_->UnregisterDisplayManagerAgent(displayManagerAgent, type));
 
-    ASSERT_EQ(DMError::DM_OK, ssm_->RegisterDisplayManagerAgent(displayManagerAgent, type));
-    ASSERT_EQ(DMError::DM_OK, ssm_->UnregisterDisplayManagerAgent(displayManagerAgent, type));
+    // ASSERT_EQ(DMError::DM_OK, ssn_->RegisterDisplayManagerAgent(displayManagerAgent, type));
+    // ASSERT_EQ(DMError::DM_OK, ssn_->UnregisterDisplayManagerAgent(displayManagerAgent, type));
+}
+
+
+/**
+ * @tc.name: UpdateActiveStatus
+ * @tc.desc: UpdateActiveStatus UpdateActiveStatus
+ * @tc.type: FUNC
+ */
+HWTEST_F(SessionProxyTest, UpdateActiveStatus01, Function | SmallTest | Level2)
+{
+    bool isActive_ = false;
+    WMError err = windowProxy_->UpdateActiveStatus(isActive_);
+    ASSERT_EQ(err, WSError::WS_DO_NOTHING);
+
+    isActive_=true;
+    WMError err = windowProxy_->UpdateActiveStatus(isActive_);
+    ASSERT_EQ(err, WSError::WS_OK);
 }
 
 /**
@@ -78,23 +105,15 @@ HWTEST_F(ScreenSessionManagerTest, RegisterDisplayManagerAgent, Function | Small
  * @tc.desc: ScreenSesionManager screen power
  * @tc.type: FUNC
  */
-HWTEST_F(ScreenSessionManagerTest, ScreenPower, Function | SmallTest | Level3)
-{
-    PowerStateChangeReason reason = PowerStateChangeReason::POWER_BUTTON;
-    ScreenPowerState state = ScreenPowerState::POWER_ON;
-    DisplayState displayState = DisplayState::ON;
+// HWTEST_F(SessionProxyTest, ScreenPower, Function | SmallTest | Level3)
+// {
+//     PowerStateChangeReason reason = PowerStateChangeReason::POWER_BUTTON;
+//     ScreenPowerState state = ScreenPowerState::POWER_ON;
+//     DisplayState displayState = DisplayState::ON;
 
-    ASSERT_EQ(false, ssm_->WakeUpBegin(reason));
-    ASSERT_EQ(false, ssm_->WakeUpEnd());
+//     ASSERT_EQ(false, ssn_->WakeUpBegin(reason));
 
-    ASSERT_EQ(false, ssm_->SuspendBegin(reason));
-    ASSERT_EQ(false, ssm_->SuspendEnd());
-
-    ASSERT_EQ(false, ssm_->SetScreenPowerForAll(state, reason));
-
-    ASSERT_EQ(true, ssm_->SetDisplayState(displayState));
-    ASSERT_EQ(DisplayState::ON, ssm_->GetDisplayState(0));
-}
+// }
 
 }
 } // namespace Rosen
