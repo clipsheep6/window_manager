879421c0c7bd9e78ac7e82886bfa64d28beb8cda
yangfei <yangfei110@huawei.com>
2023-07-27 22:15:44 +0800

    revise dialog bug
    Signed-off-by: yangfei <yangfei110@huawei.com>
    Change-Id: Ib081e9ef7ab27ef663bae168bb745dc84e7b2d82

--- window_scene/session/host/include/session.h
+++ window_scene/session/host/include/session.h
@@ -152,11 +152,13 @@ public:
 
     sptr<ScenePersistence> GetScenePersistence() const;
     void SetParentSession(const sptr<Session>& session);
+    sptr<Session> GetParentSession() const;
     void BindDialogToParentSession(const sptr<Session>& session);
     void RemoveDialogToParentSession(const sptr<Session>& session);
     std::vector<sptr<Session>> GetDialogVector() const;
     void NotifyTouchDialogTarget();
     WSError NotifyDestroy();
+    bool CheckDialogOnForeground();
 
     void SetPendingSessionToForegroundListener(const NotifyPendingSessionToForegroundFunc& func);
     WSError PendingSessionToForeground();
@@ -237,7 +239,6 @@ protected:
     sptr<ScenePersistence> scenePersistence_ = nullptr;
 
 private:
-    bool CheckDialogOnForeground();
 
     template<typename T>
     bool RegisterListenerLocked(std::vector<std::shared_ptr<T>>& holder, const std::shared_ptr<T>& listener);
@@ -275,7 +276,7 @@ private:
     sptr<IWindowEventChannel> windowEventChannel_ = nullptr;
 
     std::vector<sptr<Session>> dialogVec_;
-    sptr<Session> parentSession_;
+    sptr<Session> parentSession_ = nullptr;
 
     int32_t callingPid_ = { 0 };
     int32_t callingUid_ = { 0 };
