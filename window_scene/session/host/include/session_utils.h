ff2b6bc6c604699a137d18e62d248a154e75f187
shiwz <shiweizhi1@huawei.com>
2023-07-28 21:48:14 +0800

    填充sessionInfo
    Signed-off-by: shiwz <shiweizhi1@huawei.com>
    Change-Id: Iaf25c81d82a70ea616c5df1b7ec92c0469e9dac1

--- window_scene/session/host/include/session_utils.h
+++ window_scene/session/host/include/session_utils.h
@@ -17,9 +17,15 @@
 #define OHOS_ROSEN_WINDOW_SCENE_SESSION_UTILS_H
 
 #include "wm_common_inner.h"
+#include "iservice_registry.h"
+#include "if_system_ability_manager.h"
+#include "system_ability_definition.h"
+#include "bundle_mgr_interface.h"
 
 namespace OHOS::Rosen {
 namespace SessionUtils {
+constexpr HiviewDFX::HiLogLabel LABEL = { LOG_CORE, HILOG_DOMAIN_WINDOW, "SessionUtils" };
+constexpr int32_t UID_TRANSFORM_DIVISOR = 200000;  // local account id = uid / UID_TRANSFORM_DIVISOR
 
 inline float ToLayoutWidth(const int32_t winWidth, float vpr)
 {
@@ -52,6 +58,45 @@ inline void CalcFloatWindowRectLimits(const WindowLimits& limits, int32_t maxFlo
     maxWidth = std::min(maxWidth, static_cast<int32_t>(maxFloatingWindowSize * vpr));
     minHeight = std::max(minHeight, static_cast<int32_t>(MIN_FLOATING_HEIGHT * vpr));
     maxHeight = std::min(maxHeight, static_cast<int32_t>(maxFloatingWindowSize * vpr));
+}
+
+inline sptr<AppExecFwk::IBundleMgr> GetBundleManager()
+{
+    auto systemAbilityMgr = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    if (systemAbilityMgr == nullptr) {
+        WLOGFE("Failed to get SystemAbilityManager.");
+        return nullptr;
+    }
+    auto bmsObj = systemAbilityMgr->GetSystemAbility(BUNDLE_MGR_SERVICE_SYS_ABILITY_ID);
+    if (bmsObj == nullptr) {
+        WLOGFE("Failed to get BundleManagerService.");
+        return nullptr;
+    }
+    return iface_cast<AppExecFwk::IBundleMgr>(bmsObj);
+}
+
+inline sptr<AppExecFwk::AbilityInfo> QueryAbilityInfoFromBMS(const int32_t uId, const std::string& bundleName, const std::string& abilityName,
+    const std::string& moduleName)
+{
+    AAFwk::Want want;
+    want.SetElementName("", bundleName, abilityName, moduleName);
+    sptr<AppExecFwk::AbilityInfo> abilityInfo = new (std::nothrow) AppExecFwk::AbilityInfo();
+    if (abilityInfo == nullptr) {
+        return nullptr;
+    }
+    auto abilityInfoFlag = (AppExecFwk::AbilityInfoFlag::GET_ABILITY_INFO_WITH_APPLICATION |
+        AppExecFwk::AbilityInfoFlag::GET_ABILITY_INFO_WITH_PERMISSION |
+        AppExecFwk::AbilityInfoFlag::GET_ABILITY_INFO_WITH_METADATA);
+    auto bundleMgr = GetBundleManager();
+    if (bundleMgr == nullptr) {
+        return nullptr;
+    }
+    bool ret = bundleMgr->QueryAbilityInfo(want, abilityInfoFlag, uId, *abilityInfo);
+    if (!ret) {
+        WLOGFE("Get ability info from BMS failed!");
+        return nullptr;
+    }
+    return abilityInfo;
 }
 
 } // namespace SessionUtils
