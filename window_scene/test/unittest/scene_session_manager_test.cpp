f21ac12dd7a836958d7b85c32d4bd149d6a9ddf9
zwx1279861 <zhouxin176@h-partners.com>
2023-08-15 17:20:01 +0800

    8.15新增用例提交
    Signed-off-by: zwx1279861 <zhouxin176@h-partners.com>

--- window_scene/test/unittest/scene_session_manager_test.cpp
+++ window_scene/test/unittest/scene_session_manager_test.cpp
@@ -199,10 +199,11 @@ HWTEST_F(SceneSessionManagerTest, SetSessionContinueState, Function | SmallTest 
     auto continueState = static_cast<ContinueState>(data->ReadInt32());
     WSError result01 = ssm_->SetSessionContinueState(token, continueState);
     WSError result02 = ssm_->SetSessionContinueState(nullptr, continueState);
-    ASSERT_EQ(result01, WSError::WS_OK);
+    ASSERT_EQ(result01, WSError::WS_ERROR_INVALID_PERMISSION);
     ASSERT_EQ(result02, WSError::WS_ERROR_INVALID_PARAM);
 }
 
+/*
  * @tc.name: ConfigWindowSizeLimits01
  * @tc.desc: call ConfigWindowSizeLimits and check the systemConfig_.
  * @tc.type: FUNC
@@ -228,6 +229,225 @@ HWTEST_F(SceneSessionManagerTest, ConfigWindowSizeLimits01, Function | SmallTest
     ASSERT_EQ(sceneSessionManager->systemConfig_.miniWidthOfSubWindow_, 30);
     ASSERT_EQ(sceneSessionManager->systemConfig_.miniHeightOfSubWindow_, 40);
 }
+
+/**
+ * @tc.name: ConfigDecor
+ * @tc.desc: SceneSesionManager config decor
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigDecor, Function | SmallTest | Level3)
+{
+    WindowSceneConfig::ConfigItem* item = new WindowSceneConfig::ConfigItem;
+    int ret = 0;
+    ssm_->ConfigDecor(*item);
+    ASSERT_EQ(ret, 0);
+}
+
+/**
+ * @tc.name: ConfigWindowEffect
+ * @tc.desc: SceneSesionManager config window effect
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigWindowEffect, Function | SmallTest | Level3)
+{
+    WindowSceneConfig::ConfigItem* item = new WindowSceneConfig::ConfigItem;
+    int ret = 0;
+    ssm_->ConfigWindowEffect(*item);
+    ASSERT_EQ(ret, 0);
+}
+
+/**
+ * @tc.name: ConfigAppWindowCornerRadius
+ * @tc.desc: SceneSesionManager config app window coener radius
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigAppWindowCornerRadius, Function | SmallTest | Level3)
+{
+    float out = 0.0f;
+    WindowSceneConfig::ConfigItem item01;
+    item01.SetValue(new std::string("ConfigAppWindowCornerRadius"));
+    bool result01 = ssm_->ConfigAppWindowCornerRadius(item01, out);
+    ASSERT_EQ(result01, true);
+
+    WindowSceneConfig::ConfigItem item02;
+    item02.SetValue(new string("defaultCornerRadiusS"));
+    bool result02 = ssm_->ConfigAppWindowCornerRadius(item02, out);
+    ASSERT_EQ(result02, false);
+    ASSERT_EQ(out, 24.0f);
+}
+
+/**
+ * @tc.name: ConfigAppWindowShadow
+ * @tc.desc: SceneSesionManager config app window shadow
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigAppWindowShadow, Function | SmallTest | Level3)
+{
+    WindowSceneConfig::ConfigItem item;
+    WindowSceneConfig::ConfigItem shadowConfig;
+    WindowShadowConfig outShadow;
+    std::vector<float> floatTest = {0.0f, 0.1f, 0.2f, 0.3f};
+    bool result01 = ssm_->ConfigAppWindowShadow(shadowConfig, outShadow);
+    ASSERT_EQ(result01, true);
+
+    item.SetValue(floatTest);
+    shadowConfig.SetValue({{"radius", item}});
+    bool result02 = ssm_->ConfigAppWindowShadow(shadowConfig, outShadow);
+    ASSERT_EQ(result02, false);
+    shadowConfig.SetValue({{"alpha", item}});
+    bool result03 = ssm_->ConfigAppWindowShadow(shadowConfig, outShadow);
+    ASSERT_EQ(result03, false);
+    shadowConfig.SetValue({{"sffsetY", item}});
+    bool result04 = ssm_->ConfigAppWindowShadow(shadowConfig, outShadow);
+    ASSERT_EQ(result04, true);
+    shadowConfig.SetValue({{"sffsetX", item}});
+    bool result05 = ssm_->ConfigAppWindowShadow(shadowConfig, outShadow);
+    ASSERT_EQ(result05, false);
+
+    item.SetValue(new std::string("color"));
+    shadowConfig.SetValue({{"color", item}});
+    bool result06 = ssm_->ConfigAppWindowShadow(shadowConfig, outShadow);
+    ASSERT_EQ(result06, false);
+}
+
+/**
+ * @tc.name: ConfigKeyboardAnimation
+ * @tc.desc: SceneSesionManager config keyboard animation
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigKeyboardAnimation, Function | SmallTest | Level3)
+{
+    WindowSceneConfig::ConfigItem animationConfig;
+    WindowSceneConfig::ConfigItem itemCurve;
+    WindowSceneConfig::ConfigItem itemDurationIn;
+    WindowSceneConfig::ConfigItem itemDurationOut;
+    std::vector<int> curve = {39};
+    std::vector<int> durationIn = {39};
+    std::vector<int> durationOut = {39};
+
+    itemCurve.SetValue(curve);
+    itemCurve.SetValue({{"curve", itemCurve}});
+    itemDurationIn.SetValue(durationIn);
+    itemDurationIn.SetValue({{"durationIn", itemDurationIn}});
+    itemDurationOut.SetValue(durationOut);
+    itemDurationOut.SetValue({{"durationOut", itemDurationOut}});
+    animationConfig.SetValue({{"timing", itemCurve}, {"timing", itemDurationIn}, {"timing", itemDurationOut}});
+    
+    int ret = 0;
+    ssm_->ConfigKeyboardAnimation(animationConfig);
+    ASSERT_EQ(ret, 0);
+}
+
+/**
+ * @tc.name: ConfigWindowAnimation
+ * @tc.desc: SceneSesionManager config window animation
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigWindowAnimation, Function | SmallTest | Level3)
+{
+    WindowSceneConfig::ConfigItem windowAnimationConfig;
+    WindowSceneConfig::ConfigItem item;
+    std::vector<float> opacity = {0.1f};
+    std::vector<float> translate = {0.1f, 0.2f};
+    std::vector<float> rotation = {0.1f, 0.2f, 0.3f, 0.4f};
+    std::vector<float> scale = {0.1f, 0.2f};
+    std::vector<int> duration = {39};
+
+    item.SetValue(opacity);
+    windowAnimationConfig.SetValue({{"opacity", item}});
+    int ret = 0;
+    ssm_->ConfigWindowAnimation(windowAnimationConfig);
+    ASSERT_EQ(ret, 0);
+
+    item.SetValue(rotation);
+    windowAnimationConfig.SetValue({{"rotation", item}});
+    ssm_->ConfigWindowAnimation(windowAnimationConfig);
+    ASSERT_EQ(ret, 0);
+
+    item.SetValue(translate);
+    windowAnimationConfig.SetValue({{"translate", item}});
+    ssm_->ConfigWindowAnimation(windowAnimationConfig);
+    ASSERT_EQ(ret, 0);
+
+    item.SetValue(scale);
+    windowAnimationConfig.SetValue({{"scale", item}});
+    ssm_->ConfigWindowAnimation(windowAnimationConfig);
+    ASSERT_EQ(ret, 0);
+
+    item.SetValue(duration);
+    item.SetValue({{"duration", item}});
+    windowAnimationConfig.SetValue({{"timing", item}});
+    ssm_->ConfigWindowAnimation(windowAnimationConfig);
+    ASSERT_EQ(ret, 0);
+
+    item.SetValue(duration);
+    item.SetValue({{"curve", item}});
+    windowAnimationConfig.SetValue({{"timing", item}});
+    ssm_->ConfigWindowAnimation(windowAnimationConfig);
+    ASSERT_EQ(ret, 0);
+}
+
+/**
+ * @tc.name: ConfigStartingWindowAnimation
+ * @tc.desc: SceneSesionManager config start window animation
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerTest, ConfigStartingWindowAnimation, Function | SmallTest | Level3)
+{
+    std::vector<float> midFloat = {0.1f};
+    std::vector<int> midInt = {1};
+    WindowSceneConfig::ConfigItem middleFloat;
+    middleFloat.SetValue(midFloat);
+    WindowSceneConfig::ConfigItem middleInt;
+    middleInt.SetValue(midInt);
+    WindowSceneConfig::ConfigItem curve;
+    curve.SetValue(midFloat);
+    curve.SetValue({{"curve", curve}});
+    WindowSceneConfig::ConfigItem enableConfigItem;
+    enableConfigItem.SetValue(false);
+    std::map<std::string, WindowSceneConfig::ConfigItem> midMap = {{"duration", middleInt}, {"curve", curve}};
+    WindowSceneConfig::ConfigItem timing;
+    timing.SetValue(midMap);
+    std::map<std::string, WindowSceneConfig::ConfigItem> middleMap = {{"enable", enableConfigItem}, 
+        {"timing", timing}, {"opacityStart", middleFloat}, {"opacityEnd", middleFloat}};
+    WindowSceneConfig::ConfigItem configItem;
+    configItem.SetValue(middleMap);
+    int ret = 0;
+    ssm_->ConfigStartingWindowAnimation(configItem);
+    ASSERT_EQ(ret, 0);
+    midMap.clear();
+    middleMap.clear();
+}
+
+/**
+ * @tc.name: CreateCurve
+ * @tc.desc: SceneSesionManager create curve
+ * @tc.type: FUNC
+ */
+ HWTEST_F(SceneSessionManagerTest, CreateCurve, Function | SmallTest | Level3)
+ {
+    WindowSceneConfig::ConfigItem curveConfig;
+    std::string nodeName;
+    std::string result01 = ssm_->CreateCurve(curveConfig, nodeName);
+    ASSERT_EQ(result01, "easeOut");
+
+    curveConfig.SetValue(new std::string("userName"));
+    curveConfig.SetValue({{"name", curveConfig}});
+    std::string result02 = ssm_->CreateCurve(curveConfig, nodeName);
+    ASSERT_EQ(result02, "easeOut");
+
+    curveConfig.SetValue(new std::string("interactiveSpring"));
+    curveConfig.SetValue({{"name", curveConfig}});
+    std::string result03 = ssm_->CreateCurve(curveConfig, nodeName);
+    ASSERT_EQ(result03, "interactiveSpring");
+
+    curveConfig.SetValue(new std::string("cubic"));
+    curveConfig.SetValue({{"name", curveConfig}});
+    nodeName = "windowAnimation";
+    std::string result04 = ssm_->CreateCurve(curveConfig, nodeName);
+    ASSERT_EQ(result04, "cubic");
+ }
+
 }
 } // namespace Rosen
 } // namespace OHOS
