c631f0aa0f6036dd4bbab89fab6ff1f0690eeef7
ywx <yangwenxin9@huawei.com>
2023-08-30 18:28:49 +0800

    窗口TDD
    Signed-off-by: ywx <yangwenxin9@huawei.com>
    Change-Id: I4371092fd934c7668f09b906bc234147b8bd87d9

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -1,3 +1,4 @@
+\
 /*
  * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -419,6 +420,7 @@ void SceneSession::NotifyPropertyWhenConnect()
         WLOGFD("id: %{public}d property is nullptr", persistentId_);
         return;
     }
+    
     NotifySessionFocusableChange(property_->GetFocusable());
     NotifySessionTouchableChange(property_->GetTouchable());
     OnShowWhenLocked(IsShowWhenLocked());
