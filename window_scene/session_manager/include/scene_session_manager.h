17526551d4f7a6255ed0f28eeab404aa037cab85
liugan <liugan8@huawei.com>
2023-09-04 16:36:53 +0800

    注册状态栏监听时，立即回调，及时获取当前状态
    Signed-off-by: liugan <liugan8@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -322,6 +322,7 @@ private:
     bool isReportTaskStart_ = false;
     std::shared_ptr<RSOcclusionData> lastOcclusionData_ = std::make_shared<RSOcclusionData>();
     RSInterfaces& rsInterface_;
+    bool lastGestureNavigationEnabled_ = true;
     void RegisterSessionStateChangeNotifyManagerFunc(sptr<SceneSession>& sceneSession);
     void OnSessionStateChange(int32_t persistentId, const SessionState& state);
     sptr<ISessionChangeListener> sessionListener_;
@@ -357,6 +358,7 @@ private:
     void PreHandleCollaborator(sptr<SceneSession>& sceneSession);
     void NotifyCollaboratorAfterStart(sptr<SceneSession>& scnSession, sptr<AAFwk::SessionInfo>& scnSessionInfo);
     void UpdateCollaboratorSessionWant(sptr<SceneSession>& session);
+    void NotifyStatusBarEnabledChange(bool enable);
 };
 } // namespace OHOS::Rosen
 
