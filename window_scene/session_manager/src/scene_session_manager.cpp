71daf7ab92e9e641aa2d983a81de535821ecff71
hukai86 <mail.hk@163.com>
2023-09-09 16:26:55 +0800

    fix GetSessionSnapshot interface
    Signed-off-by: hukai86 <mail.hk@163.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -3068,7 +3068,7 @@ WSError SceneSessionManager::TerminateSessionNew(const sptr<AAFwk::SessionInfo> 
 }
 
 WSError SceneSessionManager::GetSessionSnapshot(const std::string& deviceId, int32_t persistentId,
-                                                std::shared_ptr<Media::PixelMap>& snapshot, bool isLowResolution)
+    MissionSnapshot& snapshot, bool isLowResolution)
 {
     WLOGFI("run GetSessionSnapshot");
     if (!SessionPermission::JudgeCallerIsAllowedToUseSystemAPI()) {
@@ -3087,7 +3087,7 @@ WSError SceneSessionManager::GetSessionSnapshot(const std::string& deviceId, int
             if (ret != ERR_OK) {
                 return WSError::WS_ERROR_INVALID_PARAM;
             } else {
-                snapshot = missionSnapshotPtr->snapshot;
+                snapshot.snapshot = missionSnapshotPtr->snapshot;
                 return WSError::WS_OK;
             }
         }
@@ -3096,6 +3096,11 @@ WSError SceneSessionManager::GetSessionSnapshot(const std::string& deviceId, int
             WLOGFE("fail to find session by persistentId: %{public}d", persistentId);
             return WSError::WS_ERROR_INVALID_PARAM;
         }
+        SessionInfo sessionInfo = sceneSession->GetSessionInfo();
+        if (sessionInfo) {
+            snapshot.topAbility.bundleName = sessionInfo.moduleName_;
+            snapshot.topAbility.abilityName = sessionInfo.abilityName_;
+        }
         auto oriSnapshot = sceneSession->Snapshot();
         if (oriSnapshot != nullptr) {
             if (isLowResolution) {
@@ -3103,9 +3108,9 @@ WSError SceneSessionManager::GetSessionSnapshot(const std::string& deviceId, int
                 options.size.width = oriSnapshot->GetWidth() / 2; // low resolution ratio
                 options.size.height = oriSnapshot->GetHeight() / 2; // low resolution ratio
                 std::unique_ptr<OHOS::Media::PixelMap> reducedPixelMap = OHOS::Media::PixelMap::Create(*oriSnapshot, options);
-                snapshot = std::shared_ptr<OHOS::Media::PixelMap>(reducedPixelMap.release());
+                snapshot.snapshot = std::shared_ptr<OHOS::Media::PixelMap>(reducedPixelMap.release());
             } else {
-                snapshot = oriSnapshot;
+                snapshot.snapshot = oriSnapshot;
             }
         }
         return WSError::WS_OK;
@@ -3537,7 +3542,7 @@ void SceneSessionManager::NotifyWindowInfoChange(int32_t persistentId, WindowUpd
     WLOGFI("NotifyWindowInfoChange, persistentId = %{public}d, updateType = %{public}d", persistentId, type);
     sptr<SceneSession> sceneSession = GetSceneSession(persistentId);
     if (sceneSession == nullptr) {
-        WLOGFE("GetSessionSnapshot sceneSession nullptr!");
+        WLOGFE("NotifyWindowInfoChange sceneSession nullptr!");
         return;
     }
     wptr<SceneSession> weakSceneSession(sceneSession);
@@ -3596,10 +3601,10 @@ bool SceneSessionManager::FillWindowInfo(std::vector<sptr<AccessibilityWindowInf
 
 std::string SceneSessionManager::GetSessionSnapshotFilePath(int32_t persistentId)
 {
-    WLOGFI("GetSessionSnapshot persistentId %{public}d", persistentId);
+    WLOGFI("GetSessionSnapshotFilePath persistentId %{public}d", persistentId);
     auto sceneSession = GetSceneSession(persistentId);
     if (sceneSession == nullptr) {
-        WLOGFE("GetSessionSnapshot sceneSession nullptr!");
+        WLOGFE("GetSessionSnapshotFilePath sceneSession nullptr!");
         return "";
     }
     wptr<SceneSession> weakSceneSession(sceneSession);
