633680d45f8d03db7340d41e7fedda89baa45f39
wangmiaoliang <wangmiaoliang1@huawei.com>
2023-09-28 03:13:19 +0000

    system scene background alpha
    Signed-off-by: wangmiaoliang <wangmiaoliang1@huawei.com>
    Change-Id: I6bbdfe9c317da4d094917979c37c11bc09198e45

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
@@ -46,12 +46,14 @@ private:
     static NativeValue* SetShowRecent(NativeEngine* engine, NativeCallbackInfo* info);
     static NativeValue* SetZOrder(NativeEngine* engine, NativeCallbackInfo* info);
     static NativeValue* SetPrivacyMode(NativeEngine* engine, NativeCallbackInfo* info);
+    static NativeValue* SetSystemSceneBGAlpha(NativeEngine* engine, NativeCallbackInfo* info);
 
     NativeValue* OnRegisterCallback(NativeEngine& engine, NativeCallbackInfo& info);
     NativeValue* OnUpdateNativeVisibility(NativeEngine& engine, NativeCallbackInfo& info);
     NativeValue* OnSetShowRecent(NativeEngine& engine, NativeCallbackInfo& info);
     NativeValue* OnSetZOrder(NativeEngine& engine, NativeCallbackInfo& info);
     NativeValue* OnSetPrivacyMode(NativeEngine& engine, NativeCallbackInfo& info);
+    NativeValue* OnSetSystemSceneBGAlpha(NativeEngine& engine, NativeCallbackInfo& info);
 
     bool IsCallbackRegistered(const std::string& type, NativeValue* jsListenerObject);
     bool IsCallbackTypeSupported(const std::string& type);
