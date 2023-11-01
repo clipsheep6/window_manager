74799b08bafad62a6a49c29ed4e808fb9534b8b7
zhangkai <zhangkai324@huawei.com>
2023-11-01 19:46:22 +0800

    fix async to post task
    Signed-off-by: zhangkai <zhangkai324@huawei.com>
    Change-Id: Ia332650b5ffc99b528603a3988fc14712453cb44

--- interfaces/kits/napi/window_runtime/window_napi/js_window_listener.h
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_listener.h
@@ -80,6 +80,8 @@ public:
     void OnGestureNavigationEnabledUpdate(bool enable) override;
     void OnWaterMarkFlagUpdate(bool showWaterMark) override;
     void CallJsMethod(const char* methodName, napi_value const* argv = nullptr, size_t argc = 0);
+
+    void SetMainEventHandler();
 private:
     WindowState state_ {WindowState::STATE_INITIAL};
     void LifeCycleCallBack(LifeCycleEventType eventType);
