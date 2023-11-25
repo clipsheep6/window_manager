7b89cb949d17f44bfa6af84e43bbfbbc88e3b302
zhangzhongqing <2543520998@qq.com>
2023-11-25 21:29:35 +0800

    Signed-off-by: zhangzhongqing <2543520998@qq.com>
    	modified:   wm/src/window_scene_session_impl.cpp

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -996,6 +996,12 @@ WMError WindowSceneSessionImpl::SetLayoutFullScreen(bool status)
     if (!WindowHelper::IsWindowModeSupported(property_->GetModeSupportInfo(), WindowMode::WINDOW_MODE_FULLSCREEN)) {
         return WMError::WM_ERROR_INVALID_WINDOW;
     }
+    if (property_->GetWindowMode() == WindowMode::WindowMode::WINDOW_MODE_FLOATIN ||
+        property_->GetWindowMode() == WindowMode::WINDOW_MODE_SPLIT_PRIMARY ||
+        property_->GetWindowMode() == WindowMode::WINDOW_MODE_SPLIT_SECONDARY)  &&
+        system::GetParameter("const.product.devicetype", "unknown") == "phone")) {
+        return WMError::WM_ERROR_INVALID_WINDOW;
+    }
     hostSession_->OnSessionEvent(SessionEvent::EVENT_MAXIMIZE);
     SetWindowMode(WindowMode::WINDOW_MODE_FULLSCREEN);
     WMError ret = SetLayoutFullScreenByApiVersion(status);
