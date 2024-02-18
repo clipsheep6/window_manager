d7f6e974a2b479dc3a7cb2fa7e754e5d0c05503d
xiongyonglong <xiongyonglong@huawei.com>
2024-02-18 16:17:16 +0800

    增加模态子窗口恢复

--- window_scene/session/host/src/session.cpp
+++ window_scene/session/host/src/session.cpp
@@ -813,6 +813,7 @@ WSError Session::Reconnect(const sptr<ISessionStage>& sessionStage, const sptr<I
     auto type = property->GetWindowType();
     if (windowState == WindowState::STATE_SHOWN || SessionHelper::IsSubWindow(type)) {
         isActive_ = true;
+        isDialogWindowShow_ = true;
         if (SessionHelper::IsMainWindow(type) || type == WindowType::WINDOW_TYPE_INPUT_METHOD_FLOAT) {
             UpdateSessionState(SessionState::STATE_ACTIVE);
         } else {
@@ -820,6 +821,7 @@ WSError Session::Reconnect(const sptr<ISessionStage>& sessionStage, const sptr<I
         }
     } else {
         isActive_ = false;
+        isDialogWindowShow_ = false;
         UpdateSessionState(SessionState::STATE_BACKGROUND);
     }
     bufferAvailable_ = true;
@@ -1760,6 +1762,13 @@ void Session::SetSessionStateChangeListenser(const NotifySessionStateChangeFunc&
         changedState = SessionState::STATE_BACKGROUND;
     } else if (changedState == SessionState::STATE_DISCONNECT) {
         return;
+    }
+    if(GetWindowType() == WindowType::WINDOW_TYPE_DIALOG) {
+        if(isDialogWindowShow_){
+            changedState = SessionState::STATE_FOREGROUND;
+        } else{
+            changedState = SessionState::STATE_BACKGROUND;
+        }
     }
     NotifySessionStateChange(changedState);
     WLOGFD("SetSessionStateChangeListenser, id: %{public}d, state_: %{public}d, changedState: %{public}d",
