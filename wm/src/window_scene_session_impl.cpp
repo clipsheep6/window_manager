76309e3f2872e32f850b6886b2fb6ef223b5f468
wangmiaoliang <wangmiaoliang1@huawei.com>
2024-03-06 03:25:41 +0000

    fix warn
    Signed-off-by: wangmiaoliang <wangmiaoliang1@huawei.com>
    Change-Id: I1da9dcaff69895d19db37c29765df0adc8a0daad

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -982,10 +982,10 @@ WMError WindowSceneSessionImpl::Destroy(bool needNotifyServer, bool needClearLis
     }
 
     DestroySubWindow();
-	{
+    {
         std::unique_lock<std::shared_mutex> lock(windowSessionMutex_);
         windowSessionMap_.erase(property_->GetWindowName());
-	}
+    }
     DelayedSingleton<ANRHandler>::GetInstance()->OnWindowDestroyed(property_->GetPersistentId());
     hostSession_ = nullptr;
     NotifyAfterDestroy();
