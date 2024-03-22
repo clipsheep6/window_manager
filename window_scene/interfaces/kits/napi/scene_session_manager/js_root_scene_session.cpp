4f7577b33d96518da71235f376099c9fab5e0d95
yangwenxin <yangwenxin9@huawei.com>
2024-03-22 02:32:57 +0000

    update window_scene/interfaces/kits/napi/scene_session_manager/js_root_scene_session.cpp.
    Signed-off-by: yangwenxin <yangwenxin9@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_root_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_root_scene_session.cpp
@@ -250,7 +250,7 @@ void JsRootSceneSession::PendingSessionActivation(SessionInfo& info)
     
     if (info.want != nullptr) {
         bool isNeedBackToOther = info.want->GetBoolParam(AAFwk::Want::PARAM_BACK_TO_OTHER_MISSION_STACK, false);
-        TLOGI(WmsLogTag::WMS_LIFE, "[NAPI]session: %{public}d isNeedBackToOther: %{public}d", sceneSession->GetpersistentID(), isNeedBackToOther);
+        TLOGI(WmsLogTag::WMS_LIFE, "[NAPI]session: %{public}d isNeedBackToOther: %{public}d", sceneSession->GetPersistentID(), isNeedBackToOther);
         if (isNeedBackToOther) {
             int32_t realCallerSessionId = SceneSessionManager::GetInstance().GetFocusedSession();
             if (realCallerSessionId == sceneSession->GetPersistentId()) {
