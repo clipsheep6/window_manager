4436cae9c434d8120b149dc532692671bbf3239f
黄四平 <huangsiping3@huawei.com>
2024-04-11 06:47:21 +0000

    update wmserver/include/zidl/window_manager_proxy.h.
    Signed-off-by: 黄四平 <huangsiping3@huawei.com>

--- wmserver/include/zidl/window_manager_proxy.h
+++ wmserver/include/zidl/window_manager_proxy.h
@@ -78,8 +78,6 @@ public:
     void SetMaximizeMode(MaximizeMode maximizeMode) override;
     MaximizeMode GetMaximizeMode() override;
     void GetFocusWindowInfo(FocusChangeInfo& focusInfo) override;
-    WMError GetWindowBackHomeStatus(bool &isBackHome) override;
-
 private:
     static inline BrokerDelegator<WindowManagerProxy> delegator_;
 };
