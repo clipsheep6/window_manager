06d05ee189f1677f0a8a8fbb1b4db20dd0247096
duzhihao <duzhihao2@huawei.com>
2024-04-20 14:59:11 +0800

    Decription: modify file header Copyright and resort head files IssueNo: https://gitee.com/openharmony/window_window_manager/issues/I9HZ9E?from=project-issue Feature or Bugfix: Bugfix Binary Source: No
    Signed-off-by: duzhihao <duzhihao2@huawei.com>

--- window_scene/session/screen/src/screen_session.cpp
+++ window_scene/session/screen/src/screen_session.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2012. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,11 +15,11 @@
 
 #include "session/screen/include/screen_session.h"
 
-#include "window_manager_hilog.h"
 #include <hitrace_meter.h>
 #include <surface_capture_future.h>
 #include <transaction/rs_interfaces.h>
 #include <transaction/rs_transaction.h>
+#include "window_manager_hilog.h"
 #include "dm_common.h"
 
 namespace OHOS::Rosen {
