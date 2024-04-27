c20e777feec2b0c4d4e6545205f2943a4bc96f8a
zhangyao <zhangyao114@huawei.com>
2024-04-27 10:29:47 +0000

    update window_scene/test/unittest/screen_session_manager_test.cpp.
    Signed-off-by: zhangyao <zhangyao114@huawei.com>

--- window_scene/test/unittest/screen_session_manager_test.cpp
+++ window_scene/test/unittest/screen_session_manager_test.cpp
@@ -171,10 +171,10 @@ HWTEST_F(ScreenSessionManagerTest, SetScreenPowerForAll, Function | SmallTest | 
 
     PowerStateChangeReason reason = PowerStateChangeReason::STATE_CHANGE_REASON_POWER_KEY;
     ScreenPowerState state = ScreenPowerState::POWER_ON;
-    ASSERT_EQ(true, ssm_->SetScreenPowerForAll(state, reason));
+    ASSERT_EQ(true, ssm_->SetScreenPowerForAllstate, reason));
 
     reason = PowerStateChangeReason::STATE_CHANGE_REASON_SWITCH;
-    ASSERT_EQ(true, ssm_->SetScreenPowerForAll(reason));
+    ASSERT_EQ(true, ssm_->SetScreenPowerForAllstate, reason));
 
     reason = PowerStateChangeReason::STATE_CHANGE_REASON_PRE_BRIGHT;
     ASSERT_EQ(true, ssm_->SetScreenPowerForAll(state, reason));
