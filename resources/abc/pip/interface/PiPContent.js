42fb64e92976525ea811f92ca1ad360a7545afc1
Ning Yuanfeng <Nyuanfeng@163.com>
2024-05-30 11:13:31 +0800

    使用nodeContent支持画中画自定义UI
    Signed-off-by: Ning Yuanfeng <Nyuanfeng@163.com>

--- resources/abc/pip/interface/PiPContent.js
+++ resources/abc/pip/interface/PiPContent.js
@@ -20,8 +20,9 @@ const TAG = 'PiPContent';
 export class PiPContent extends ViewPU {
     constructor(e, o, t, n = -1, i = void 0) {
         super(e, t, n);
-        "function" === typeof i && (this.paramsGenerator_ = i);
+        'function' === typeof i && (this.paramsGenerator_ = i);
         this.xComponentController = new XComponentController;
+        this.nodeContent = null;
         this.xComponentId = 'pipContent';
         this.xComponentType = 'surface';
         this.setInitiallyProvidedValue(o);
@@ -31,12 +32,17 @@ export class PiPContent extends ViewPU {
         void 0 !== e.xComponentController && (this.xComponentController = e.xComponentController);
         void 0 !== e.xComponentId && (this.xComponentId = e.xComponentId);
         void 0 !== e.xComponentType && (this.xComponentType = e.xComponentType);
+        void 0 !== e.nodeContent && (this.nodeContent = e.nodeContent);
     }
 
     updateStateVars(e) {
     }
 
     purgeVariableDependenciesOnElmtId(e) {
+    }
+
+    aboutToAppear() {
+        this.nodeContent = pip.getCustomUIController();
     }
 
     aboutToBeDeleted() {
@@ -54,13 +60,27 @@ export class PiPContent extends ViewPU {
                 id: this.xComponentId,
                 type: this.xComponentType,
                 controller: this.xComponentController
-            }, "pipContent_XComponent");
+            }, 'pipContent_XComponent');
             XComponent.onLoad((() => {
                 pip.initXComponentController(this.xComponentController);
                 console.debug(TAG, 'XComponent onLoad done');
             }));
             XComponent.size({ width: '100%', height: '100%' });
         }), XComponent);
+        this.observeComponentCreation2((d, e) => {
+            If.create();
+            if (this.nodeContent != null) {
+                this.ifElseBranchUpdateFunction(0, () => {
+                    this.observeComponentCreation2((i, j) => {
+                        ContentSlot.create(this.nodeContent);
+                    }, ContentSlot);
+                });
+            } else {
+                this.ifElseBranchUpdateFunction(1, ()=> {
+                });
+            }
+        }, If);
+        If.pop();
         Stack.pop();
     }
 
