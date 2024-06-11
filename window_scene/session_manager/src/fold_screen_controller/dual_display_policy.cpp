6fba4872a93adadda41d569603482fcb69726ce9
yellow sky <huangtian10@huawei.com>
2024-06-11 21:09:15 +0800

    Decription: 折叠开合直接调用RS上下电
    IssueNo:  https://gitee.com/huangtian0406/drivers_hdf_core/issues/IA4I3T
    
    Signed-off-by: yellow sky <huangtian10@huawei.com>

--- window_scene/session_manager/src/fold_screen_controller/dual_display_policy.cpp
+++ window_scene/session_manager/src/fold_screen_controller/dual_display_policy.cpp
@@ -259,8 +259,8 @@ void DualDisplayPolicy::ChangeScreenDisplayModeInner(sptr<ScreenSession> screenS
         TLOGI(WmsLogTag::DMS, "ChangeScreenDisplayMode: off screenId: %{public}" PRIu64 "", offScreenId);
         screenId_ = offScreenId;
         ScreenSessionManager::GetInstance().SetKeyguardDrawnDoneFlag(false);
-        ScreenSessionManager::GetInstance().SetScreenPower(ScreenPowerStatus::POWER_STATUS_OFF,
-            PowerStateChangeReason::STATE_CHANGE_REASON_DISPLAY_SWITCH);
+        rsInterface_.SetScreenPowerStatus(foldScreenController_->GetCurrentScreenId(),
+            ScreenPowerStatus::POWER_STATUS_OFF);
     };
     screenPowerTaskScheduler_->PostAsyncTask(taskScreenOff, "screenOffTask");
     AddOrRemoveDisplayNodeToTree(offScreenId, REMOVE_DISPLAY_NODE);
@@ -270,8 +270,8 @@ void DualDisplayPolicy::ChangeScreenDisplayModeInner(sptr<ScreenSession> screenS
         screenId_ = onScreenId;
         if (isScreenOn) {
             ScreenSessionManager::GetInstance().SetKeyguardDrawnDoneFlag(false);
-            ScreenSessionManager::GetInstance().SetScreenPower(ScreenPowerStatus::POWER_STATUS_ON,
-                PowerStateChangeReason::STATE_CHANGE_REASON_DISPLAY_SWITCH);
+            rsInterface_.SetScreenPowerStatus(foldScreenController_->GetCurrentScreenId(),
+                ScreenPowerStatus::POWER_STATUS_ON);
             PowerMgr::PowerMgrClient::GetInstance().RefreshActivity();
         } else {
             PowerMgr::PowerMgrClient::GetInstance().WakeupDevice();
