90f5463c0d59c2cc704aaf4c7c695a2cd42268ee
luyujing <luyujing2@huawei.com>
2024-07-03 01:18:20 +0000

    update wmserver/include/window_common_event.h.
    Signed-off-by: luyujing <luyujing2@huawei.com>

--- wmserver/include/window_common_event.h
+++ wmserver/include/window_common_event.h
@@ -51,6 +51,8 @@ private:
     };
     void SubscriberEventInner(int retry);
     void HandleAccountSwitched(const EventFwk::CommonEventData& data) const;
+
+    std::map<std::string, CommonEventAction> eventCodeMap_;
     std::shared_ptr<EventSubscriber> subscriber_;
     std::shared_ptr<AppExecFwk::EventHandler> eventHandler_;
 };
