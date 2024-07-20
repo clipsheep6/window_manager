12dcc20f9d9560bc4cb1f3b43ef5cc76a870f743
liusensen <liusen43@huawei.com>
2024-07-20 10:53:12 +0800

    SystemAPI新增权限校验
    Signed-off-by: liusensen <liusen43@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -987,7 +987,8 @@ napi_value JsWindow::OnShowWithAnimation(napi_env env, napi_callback_info info)
         } else {
             auto winType = windowToken_->GetType();
             if (!WindowHelper::IsSystemWindow(winType)) {
-                WLOGFE("window Type %{public}u is not supported", static_cast<uint32_t>(winType));
+                TLOGE(WmsLogTag::WMS_LIFE,
+                    "window Type %{public}u is not supported", static_cast<uint32_t>(winType));
                 errCode = WmErrorCode::WM_ERROR_INVALID_CALLING;
             }
         }
@@ -1173,7 +1174,8 @@ napi_value JsWindow::OnHideWithAnimation(napi_env env, napi_callback_info info)
         if (windowToken_) {
             auto winType = windowToken_->GetType();
             if (!WindowHelper::IsSystemWindow(winType)) {
-                WLOGFE("window Type %{public}u is not supported", static_cast<uint32_t>(winType));
+                TLOGE(WmsLogTag::WMS_LIFE,
+                    "window Type %{public}u is not supported", static_cast<uint32_t>(winType));
                 errCode = WmErrorCode::WM_ERROR_INVALID_CALLING;
             }
         } else {
