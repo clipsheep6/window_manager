cf5b0d9452683c9197e80f0632295915db6e5200
yyuehao <yangyuehao@huawei.com>
2024-07-29 02:01:30 +0000

    update window_scene/session_manager/include/scene_session_manager.h. 修改代码规范
    Signed-off-by: yyuehao <yangyuehao@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -412,7 +412,7 @@ private:
     void InitSceneSession(sptr<SceneSession>& sceneSession, const SessionInfo& sessionInfo,
         const sptr<WindowSessionProperty>& property);
 
-    void dumpAllSessionFocusableInfo(int32_t persistentId);
+    void DumpAllSessionFocusableInfo(int32_t persistentId);
     sptr<SceneSession> GetNextFocusableSession(int32_t persistentId);
     sptr<SceneSession> GetTopNearestBlockingFocusSession(uint32_t zOrder, bool includingAppSession);
     sptr<SceneSession> GetTopFocusableNonAppSession();
