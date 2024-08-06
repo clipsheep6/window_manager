d194535bc4f1f0ed1d32b3dfc3f3c080b4c6787a
yangwenxin <yangwenxin9@huawei.com>
2024-08-06 06:56:42 +0000

    update wm/src/window_impl.cpp.
    Signed-off-by: yangwenxin <yangwenxin9@huawei.com>

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -667,7 +667,10 @@ WMError WindowImpl::SetUIContentInner(const std::string& contentInfo, napi_env e
         float virtualPixelRatio = display->GetVirtualPixelRatio();
         auto displayInfo = display->GetDisplayInfo();
         config.SetDensity(virtualPixelRatio);
-        config.SetOrientation(static_cast<int32_t>(displayInfo->GetDisplayOrientation()));
+        if (displayInfo != nullptr) {
+            config.SetOrientation(static_cast<int32_t>(displayInfo->GetDisplayOrientation()));
+            TLOGI("notify window orientation change end");
+        }
         uiContent_->UpdateViewportConfig(config, WindowSizeChangeReason::UNDEFINED, nullptr);
         WLOGFD("notify uiContent window size change end");
     }
